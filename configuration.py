99f118980d3576693c6ac27616cbb8cf35ff19b4
Andrey <77066262+Chushenko-AV@users.noreply.github.com>
2021-03-17 09:24:56 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,10 @@
+TOKEN = '1582727823:AAGj_v642aOx6hOdubNV0jnyyGyUurLaGmk'
+
+keys = {
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'доллар': 'USD',
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+
+}
